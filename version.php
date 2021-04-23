1cd05d66ec1fb691068aef5048dd9bb447716800
JotaDF <j.wilson.df@gmail.com>
2021-04-23 11:07:16 -0300

    Update version.php

--- version.php
+++ version.php
@@ -30,5 +30,5 @@ $plugin->component = 'availability_game';
 $plugin->release   = '1.0.0';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'block_game'  => 2021033030,
+    'block_game'  => 2021033029,
 );
